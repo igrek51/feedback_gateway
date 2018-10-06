30a1f9e658e8ea50eb289fc30c107b148f06c3d0
igrek51 <igrek.s@o2.pl>
2018-10-06 13:59:21 +0200

    add ip address to message

--- contact/views/views_send.py
+++ contact/views/views_send.py
@@ -9,7 +9,13 @@ from django.views.decorators.csrf import csrf_exempt
 from contact.models import ContactMessage
 
 
-# Create your views here.
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 
 @csrf_exempt
@@ -31,6 +37,8 @@ def send_message(request):
     # Save the object into the database
     cm.save()
 
+    ip = get_client_ip(request)
+
     server = smtplib.SMTP('127.0.0.1', 25)
     server.set_debuglevel(1)
     from_address = 'songbook@vps544895.ovh.net'
@@ -39,7 +47,7 @@ def send_message(request):
     utc_now = datetime.datetime.now(pytz.timezone('UTC'))
     local_now = utc_now.astimezone(pytz.timezone('Europe/Warsaw'))
     mdatetime = local_now.strftime('%Y-%m-%d %H:%M:%S')
-    text = 'Timestamp: %s\nHere is the feedback message from %s:\n%s' % (mdatetime, author, message)
+    text = 'Timestamp: %s\nIP Address: %s\nHere is the feedback message from %s:\n%s' % (mdatetime, ip, author, message)
     message = """To: {}\nFrom: {}\nSubject: {}\n\n{}""".format(to_address, from_address, subject, text)
     server.sendmail(from_address, to_address, message.encode('utf-8'))
     server.quit()
